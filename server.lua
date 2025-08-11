7e2e1180c31b4dbaf95016f11077e034e3f4e957
! 𝕀𝕥𝕫𝔹𝕒𝕞𝕕𝕒𝕕 :)🎈 <itzbamdad@gmail.com>
2025-08-11 11:08:02 +0330

    Script Files | فایل های اسکریپت

--- server.lua
+++ server.lua
@@ -1,13 +1,2 @@
-local NDCore = exports["ND_Core"]
 
-RegisterNetEvent("ND_Fuel:pay", function(amount)
-    local src = source
-    local player = NDCore:getPlayer(src)
-    if not player then return end
 
-    player.deductMoney("bank", math.floor(amount), "Gas Station")
-    player.notify({
-        title = ("Paid: $%.2f for gas."):format(amount),
-        type = "inform"
-    })
-end)
