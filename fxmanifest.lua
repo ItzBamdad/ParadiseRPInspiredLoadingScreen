0b17ca73fc5c76e28e6f0a63b9eeffa66f6f419e
! 𝕀𝕥𝕫𝔹𝕒𝕞𝕕𝕒𝕕 :)🎈 <itzbamdad@gmail.com>
2025-08-11 11:00:43 +0330

    Script Files | فایل های اسکریپت
    توجه: کیفیت ویدیو جهت آپلود آسان تر کاهش پیدا کرده
    Info: The Video Quality Has Been Lowered For Ease Of Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+loadscreen_manual_shutdown "yes"
+
+client_script "client.lua"
+
+fx_version "bodacious"
+
+game "gta5"
+
+loadscreen 'ui.html'
+
+files {
+  'ui.html',
+  'index.js',
+  'index.css',
+  's6.webm',
+  's6.mp3',
+}
